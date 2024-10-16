9dc60db1677f170187769dae30e7c1bd33584c9d
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2024-10-16 20:44:48 +0100

    updating allowed hosts

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-o14jr6#uiv+gid3#15a)r&+4g_q4gdg3@rkyw7$pl(a6(hk)+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tobikasaba.pythonanywhere.com']
 
 # Application definition
 
